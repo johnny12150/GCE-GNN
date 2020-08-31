c1d168050a4e39c5b9d44f22f5838b834e33b0ca
Wade <johnny12150@gmail.com>
2020-08-31 10:43:18 +0800

    更新assign x/feature的方式

--- datasets/build_global_graph.py
+++ datasets/build_global_graph.py
@@ -28,6 +28,7 @@ for i, seq in tqdm(enumerate(train[0])):
                 graph_node[node]+=seq[j:j+epsilon]
             else:
                 graph_node[node]+=seq[j:len(seq)]
+graph_node = dict(sorted(graph_node.items()))  # sort dict by keys
 
 edge_lists = []
 for i, k in tqdm(enumerate(graph_node.keys())):
@@ -37,35 +38,47 @@ for i, k in tqdm(enumerate(graph_node.keys())):
     if len(graph_node[k]) > 0:
         edge_lists+=[[k, j] for j in graph_node[k]]
 edge_lists = [list(x) for x in set(tuple(x) for x in edge_lists)]  # uniques lists in list
+
 # edge_lists += [x[::-1] for x in edge_lists]  # 為了無向圖, 所有edge list都要是雙向
 # edge_lists = [list(x) for x in set(tuple(x) for x in edge_lists)]
 # edge_index = torch.tensor(edge_lists, dtype=torch.long)
 # data2 = Data(edge_index=edge_index.t().contiguous())
 
 # edge_lists = [x for x in edge_lists if not x[0]==x[1]]  # remove self loop
-l = np.array(edge_lists).transpose()
+l = np.array(edge_lists)  # sort by first then second column
+# https://stackoverflow.com/a/38194077/12859133
+l = l[l[:, 1].argsort()]
+l = l[l[:, 0].argsort(kind='mergesort')]
+l = l.transpose()
 r = np.array([l[1, :], l[0, :]])  # 建無向圖
-# x = [[l[0]] for l in edge_lists]  # get node id from edge index
-# x = torch.tensor(x, dtype=torch.float)  # fixme 應該只有304個
+# get node id from edge index
+x = []
+for col in r.T:
+    # 從edge_index中找unique node
+    if col[0] not in x:
+        x.append(col[0])
+    if col[1] not in x:
+        x.append(col[1])
+x = torch.tensor(x, dtype=torch.float)
 edge_index = torch.from_numpy(r).long()
 
 # todo 全部有 309nodes, 但只有304個有edge, 要reindex?
 # 建無向圖
-x = torch.tensor(unique_nodes).long()  # edge index內就是node id
-# x = torch.arange(l[0].max()+1).long()
-data3 = Data(x, edge_index)  # 假設x是從node id 0 開始遞增
+# x = torch.tensor(unique_nodes).long()  # edge index內就是node id
+# x = torch.arange(1, l[0].max()+1).long()
+data = Data(x, edge_index)  # 假設x是從node id 0 開始遞增
+d = data.edge_index.data.numpy()
 
 # 建nx的無向圖
 G = nx.Graph()
 G.add_edges_from(edge_lists)
 # networkx無向圖轉Data
-data = from_networkx(G)
-data.x = torch.tensor(list(G.nodes)).unsqueeze(-1)  # 不一定對的上, 因為node id可能早被重新編碼
+data3 = from_networkx(G)
+data3.x = torch.tensor(list(G.nodes)).unsqueeze(-1)  # 不一定對的上, 因為node id可能早被重新編碼
 
 # 很費時, 建好graph就直接存檔(networkx(用Data直接轉) + Data)
 torch.save(data, 'global_graph.pt')
 
-# 用link prediction train
 
 # 如果是dynamic版的話:
 # 用Data()建T張獨立的graph, 不要把這graph放進dataloader => 各自train GAT到T emb
